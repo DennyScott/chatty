d3eba0492bd45a35504f5a92ce3049339ceb5475
Denny Scott <dennyscott301@gmail.com>
2017-05-14 00:37:40 -0500

    Added base graphql server, and basic react native app

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+      "react/jsx-filename-extension": [
+        1, { "extensions": [".js", ".jsx"] }
+      ],
+      "react/require-default-props": [0],
+    },
+};
